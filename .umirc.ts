9a1fb694cb6b48a361e3afbf2b5d0af5407de19c
2016-huanglins <huanglins@foxmail.com>
2020-12-21 11:28:40 +0800

    add gbhp cli client

--- .umirc.ts
+++ .umirc.ts
@@ -86,6 +86,10 @@ export default defineConfig({
         ],
       },
       {
+        title: 'Gbhp CLI Client',
+        children: ['developer/cli/dump'],
+      },
+      {
         title: 'Tool',
         children: ['developer/tool'],
       },
@@ -103,6 +107,10 @@ export default defineConfig({
           'developer/verify',
           'developer/local',
         ],
+      },
+      {
+        title: 'Gbhp 命令行客户端',
+        children: ['developer/cli/dump'],
       },
       {
         title: '工具',
